091169a7ef46546c253a201fe41c33539b19f8bf
bychuang <bychuang@ucsc.edu>
2023-05-16 22:51:47 -0700

    added a few comments on how to complete bigrams

--- ngram.py
+++ ngram.py
@@ -5,9 +5,10 @@ import numpy as np
 
 class FeatureExtractor(object):
     def __init__(self):
-        self.grams = {}
+        self.grams = [] #just save these as a strings and w1|w2|w3 for bigrams/trigrams for easier debugging.
+        self.token_prob = [] #should be changed to token count tbh but I'm too lazy
         self.word_count = 0
-        self.start_tokens = 0
+        
     def fit(self, text_set):
         pass
     def transform(self, text):
@@ -19,14 +20,12 @@ class FeatureExtractor(object):
 class UnigramFeature():
     def __init__(self):
         self.grams = []
-        self.token_prob = [] #? Apparently we don't include <START> in these?
-                             #? just calculate it and assume its an accident if you use it
-        self.token_counts = []
-        self.word_count = 0
-        self.start_tokens = 0 #to calculate perplexity (subtract from total)
-        self.unknown_index = -1
+        self.token_prob = [] #? Apparently we don't include <START> in these probability calculations
+                             #? just calculate it and assume its an accident if you use the <START> probability
+        self.token_counts = [] #useful in building bigrams
+        self.word_count = 0 #TODO remove this. Not needed since M does not refer to the training data
         self.start_index = -1
-        self.stop_index = -1
+        self.stop_index = -1 #? not sure if this is needed.
     #this passes a file instead of the text.
 
     def fit(self, train_file):
@@ -36,10 +35,9 @@ class UnigramFeature():
         tokens["<STOP>"] = 0
         for l in train_file:
             #print(l)
-            self.start_tokens += 1
             tokens["<START>"] += 1
             tokens["<STOP>"] += 1
-            self.word_count += 2
+            self.word_count += 1
             #print(l)
             for t in l.split(" "):
                 t = t.rstrip("\n")
@@ -74,7 +72,7 @@ class UnigramFeature():
 
         #! calculate probability
         for i in range(len(self.grams)):
-            p = self.token_counts[i]/(self.word_count-self.start_tokens)
+            p = self.token_counts[i]/(self.word_count)
             self.token_prob.append(p)
         #print(self.token_prob)
         #print(tokens)
@@ -106,7 +104,55 @@ class UnigramFeature():
 
 class BigramFeature(FeatureExtractor):
     def __init__(self):
-        self.bigrams = {}
+        self.grams = []
+        self.token_prob = [] #should be changed to token count tbh but I'm too lazy
+        self.start_tokens = 0 #basically irrelevant.
+        self.word_count = 0
+        self.unigrams = UnigramFeature() #!This may provide a useful shortcut.
+    #to split the sentence into a list of bigrams
+    def sentence_splitter(self, sentence):
+        tokens = sentence.split(" ")
+        bigram_split = []
+        for i in range(1, len(tokens)):
+                #! the delimiter to find w_i and w_{i-1} is "<|>"
+                bigram_split.append(tokens[i]+"<|>"+tokens[i-1])
+        return bigram_split
+    
+    def fit(self, train_file):
+        #the values represent the number of observed outcomes under the key.
+        #useful for probability calculation
+        #TODO I'm thinking you can just make a unigram class in here and then use that
+        #TODO to parse everything since unknowns are recorded as (<UNK>|token) instead
+        #TODO of straight unknowns
+        self.unigrams.fit(train_file) #use this to replace OOV in train_file with <UNK>
+        
+        bigrams = {}
+        start_tokens = 0
+        for Xi in train_file:
+            Xi = Xi.rstrip("\n")
+            Xi = Xi + " <STOP>"
+            #count unigram tokens for future probabilities
+            for wi in Xi:
+                start_tokens += 0
+                if wi not in unigrams.keys():
+                    unigrams[wi] = 0
+                unigrams[wi] += 1
+            Xi_tokenized = "<START> " + Xi
+            Xi_tokenized = self.sentence_splitter(Xi_tokenized)
+            print(Xi_tokenized)
+            for wi in Xi_tokenized:
+                if wi not in bigrams.keys():
+                    bigrams[wi] = 0
+                bigrams[wi] += 1
+        #calculate probabilities and store into class values
+                    
+            
+            
+                
+                
+
+
+
 
 class TrigramFeature(FeatureExtractor):
     def __init__(self):
