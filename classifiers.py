30bb9d3a8eefcccb0a651ab9b7f480b1633a6154
bychuang <bychuang@ucsc.edu>
2023-05-27 00:33:53 -0700

    added nerd argparsing

--- classifiers.py
+++ classifiers.py
@@ -2,19 +2,20 @@ import numpy as np
 import math
 from ngram import *
 
-class MaxLikelihoodEst():
-    #this should still work with bigram and trigrams, but you need to transform it.
-    def eval_MLE(self, ngram, Xi): 
+
+class MaxLikelihoodEst:
+    # this should still work with bigram and trigrams, but you need to transform it.
+    def eval_MLE(self, ngram, Xi):
         prob = 0
-        #print(Xi)
+        # print(Xi)
         for wi in Xi:
             if ngram.get_prob(wi) == 0:
                 return float("-inf")
             prob += math.log(ngram.get_prob(wi), 2)
-        #print(f"MLE:", prob)
+        # print(f"MLE:", prob)
         return prob
 
-    #takes 
+    # takes
     def eval_perplexity(self, ngram_feats, X):
         sum = 0
         corpus_count = 0
@@ -24,7 +25,7 @@ class MaxLikelihoodEst():
             if curr_mle == 0:
                 return float("inf")
             sum += curr_mle
-        print("corpus count:", corpus_count)
+        # print("corpus count:", corpus_count)
         sum *= -1 / corpus_count
-        print(sum)
+        # print(sum)
         return 2**sum
--- main.py
+++ main.py
@@ -5,11 +5,9 @@ import numpy as np
 import time
 import argparse
 
-#copied from asgn1 starter code
-# def accuracy(pred, labels):
-#     correct = (np.array(pred) == np.array(labels)).sum()
-#     accuracy = correct/len(pred)
-#     print("Accuracy: %i / %i = %.4f " %(correct, len(pred), correct/len(pred)))
+BILLION_TEST_FILE = "data/1b_benchmark.test.tokens"
+BILLION_TRAIN_FILE = "data/1b_benchmark.train.tokens"
+BILLION_DEV_FILE = "data/1b_benchmark.dev.tokens"
 
 def test_perplexity_phrase(mle, ngram_features):
     phrase = ["HDTV ."]
@@ -21,15 +19,18 @@ def test_perplexity_phrase(mle, ngram_features):
 def calc_perplexity(mle, ngram_features, text_file):
     tokenized = ngram_features.transform_list(text_file)
     perp = mle.eval_perplexity(ngram_features, tokenized)
-    print("perplexity:", perp)
-    pass
+    return perp
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--smooth", "-s", action="store_true", help="enable smoothing for the MLE")
     parser.add_argument("--ngram", "-g", type=int, default=1, help="choose between unigram(1), bigram(2), and trigram(3)")
-    parser.add_argument("--train", "-t", default="data/1b_benchmark.train.tokens", help="choose training token file")
-    parser.add_argument("--predict", "-p", default="data/1b_benchmark.dev.tokens", help="choose token file to predict on")
+    parser.add_argument("--train", "-t", default=BILLION_TRAIN_FILE, help="choose training token file")
+    parser.add_argument("--predict", "-p", default=BILLION_DEV_FILE, help="choose token file to predict on")
+    parser.add_argument("--full", action="store_true", help="find perplexities of sanity, train, and dev tokens (excludes test)")
+    parser.add_argument("--test", action="store_true", help="find perplexities of test tokens")
+    parser.add_argument("--linear", action="store_true", help="toggles linear interpolation (superseeds add-1 smoothing)")
+    parser.add_argument("--smooth", "-s", action="store_true", help="enable smoothing for the MLE")
+    parser.add_argument("--sanity", action="store_true", help="test case for \"HDTV .\"")
     parser.add_argument("--alpha", "-a", default = 0, type=int, help="Choose value for add smoothing")
     args = parser.parse_args()
 
@@ -43,29 +44,56 @@ def main():
     else:
         print("ERROR: ngram must be 1, 2 or 3")
         return 1
-    ngram_features.set_alpha(args.alpha)
+    if args.linear and args.alpha > 0:
+        print("WARNING: --alpha negated by --linear (conflicting flags)")
+    if not args.linear:
+        ngram_features.set_alpha(args.alpha)
     train_file_name = args.train
     predict_file_name = args.predict
 
     mle = MaxLikelihoodEst()
 
     train_file = open(train_file_name, "r")
-    predict_file = open(predict_file_name, "r")
-    #predict_file = open(predict_file_name, "r")
 
     ngram_features.fit(train_file)
-    #print(ngram_features.word_count)
-    test_perplexity_phrase(mle, ngram_features)
+    train_file.close()
+    print("----------------------------------------------------")
+    print("")
+    print("")
 
-    #calc_perplexity(mle, ngram_features, predict_file)
-    calc_perplexity(mle, ngram_features, predict_file)
+    if args.sanity:
+        test_perplexity_phrase(mle, ngram_features)
+        print("")
 
-    #run this again to make sure the add 1 smoothing does not change when adding more tokens
-    test_perplexity_phrase(mle, ngram_features)
+    if args.full:
+        if not args.sanity:
+            test_perplexity_phrase(mle, ngram_features)
+            print("")
+        
+        train_predict = open(BILLION_TRAIN_FILE, "r")
+        val = calc_perplexity(mle, ngram_features, train_predict)
+        print(BILLION_TRAIN_FILE, "perplexity:", val)
+        print("")
+        train_predict.close()
 
-    train_file.seek(0)
+        dev_predict = open(BILLION_DEV_FILE, "r")
+        val = calc_perplexity(mle, ngram_features, dev_predict)
+        print(BILLION_DEV_FILE, "perplexity:", val)
+        print("")
+        dev_predict.close()
 
-    
+    if not args.full:
+        predict_file = open(predict_file_name, "r")
+        perp = calc_perplexity(mle, ngram_features, predict_file)
+        print(args.predict, "perplexity:", perp)
+        predict_file.close()
+
+    if args.test:
+        test_file = open(BILLION_TEST_FILE, "r")
+        perp = calc_perplexity(mle, ngram_features, test_file)
+        print(BILLION_TEST_FILE, "perplexity:", perp)
+        test_file.close()
+
 
     return 0
 
